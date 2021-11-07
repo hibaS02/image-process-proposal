1b414896bbed117ac3a3e9d1acd9024e1c5f5c1c
hibaS02 <hiba.021@outlook.com>
2021-11-07 17:06:16 +0200

    Updates

---
+++ precache-manifest.d46fd98912112a7508fd8eb0220cdad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f102ac7e36210b8155bebd6df45e699",
+    "url": "/image-process-proposal/index.html"
+  },
+  {
+    "revision": "6bc9828388e65bcaa1ba",
+    "url": "/image-process-proposal/static/css/main.b5d5a46f.chunk.css"
+  },
+  {
+    "revision": "97286b774fe8abce6106",
+    "url": "/image-process-proposal/static/js/2.6b343ae6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/image-process-proposal/static/js/2.6b343ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc9828388e65bcaa1ba",
+    "url": "/image-process-proposal/static/js/main.938180f0.chunk.js"
+  },
+  {
+    "revision": "5e1ba1e97a1d5058222c",
+    "url": "/image-process-proposal/static/js/runtime-main.cb65fa6f.js"
+  },
+  {
+    "revision": "3c1c8aaa4891ec24c21609f6ed774fba",
+    "url": "/image-process-proposal/static/media/bau-logo.3c1c8aaa.jpg"
+  }
+]);
